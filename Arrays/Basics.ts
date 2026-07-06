b49eba12197a72f33d6e05382c237e50205acdc0
Harsh Srivastava <harsh.srivastav@seceon.com>
2026-07-06 12:49:16 +0530

    Adding Arrays code

---
+++ Arrays/Basics.ts
@@ -0,0 +1,7 @@
+//Creating and assigning values to array
+
+let names: string []=["John","Smit","Peter","Scott"]
+
+//Printing data assigned to array
+
+console.log(names)
---
+++ Arrays/UsingArrayType.ts
@@ -0,0 +1,27 @@
+//Using genric array type to declare and store data
+
+let empName:Array<string>=["John","Smith","Scott","Peter"]
+
+console.log(empName)
+
+
+//Storing multiple types of data
+
+
+let data:Array<any>=["Harsh",1,true,null]
+
+console.log(data)
+
+
+//Getting specific value from data array
+console.log(data[2])
+
+//Getting all vlaues from data array
+
+for(let i=0;i<data.length;i++)
+{
+  console.log("Value at ",i,"position is : ",data[i])
+}
+
+
+
