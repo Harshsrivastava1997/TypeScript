24f299bb09434400d96d9edbf4e0f1bb26a82a98
Harsh Srivastava <harsh.srivastav@seceon.com>
2026-07-07 12:36:27 +0530

    Adding tupe array codes

--- Arrays/Tuples.ts
+++ Arrays/Tuples.ts
@@ -17,3 +17,19 @@ for(let i=0;i<values.length;i++)
 {
     console.log("Element at ",i,"position in values tuple is : ",values[i])
 }
+
+//Reading data using for-of loop
+let dataset:[string,number,boolean,string,number]=["Srivastav",1001,false,"test",2002]
+
+for(let data of dataset)
+{
+    console.log("For-of loop output ",data)
+}
+
+//Reading data using for-in loop
+let dataset1:[string,number,boolean,string,number]=["Tesrt",10001,false,"tewerst",20024]
+
+for(let i in dataset1)
+{
+    console.log("For-in loop output at ", i, "postion is : ", dataset1[i])
+}
---
+++ Arrays/TuplesArray.ts
@@ -0,0 +1,10 @@
+let students:[number,string][]=[[101,"John"],[102,"Peter"],[103,"Scott"]]
+
+console.log(students[0])
+
+//I want to extract 101 only
+
+let tp=students[0]
+
+console.log(tp[0])
+
